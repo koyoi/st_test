f4fc5697c0eea603984b9032faa4ae12f157925e
koyoi <mayura@s8.xrea.com>
2022-10-14 14:15:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file
+    )
+    
